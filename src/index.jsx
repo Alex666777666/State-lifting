c6de519d93f60cc49c16e3c77bb1cf6df4a0ae89
Alex666777666 <aleksfnaf@gmail.com>
2023-04-17 22:16:32 +0300

    State-lifting

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Page from './App.jsx'
+import './index.scss'
+
+ReactDOM.render(<Page userId='github' />, document.querySelector('#root'))
